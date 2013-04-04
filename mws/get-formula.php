bc1302f1acb79cf5ddfcfc3acf04a157545b254e
Corneliu Claudiu Prodescu <cprodescu@gmail.com>
2013-04-04 17:08:31 +0200

    Removed hard-coded search.mathweb.org URLs

--- mws/get-formula.php
+++ mws/get-formula.php
@@ -1,17 +1,11 @@
 <?php
 if (isset($_GET["formula-url"])) {
-$ch = curl_init($_GET["formula-url"]);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-header('Content-Type: text/xml');
+  $ch = curl_init($_GET["formula-url"]);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  header('Content-Type: text/xml');
+  $result = curl_exec($ch);
+  curl_close($ch);
+  header('Access-Control-Allow-Origin: *');
+  print($result);
 }
-else {
-$ch = curl_init("http://arxivdemo.mathweb.org/convert.php");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, array('mathml' => '<math xmlns="http://www.w3.org/1998/Math/MathML"><ci>HELLO</ci></math>', 'notex' => '\cos\pi', 'profile' => 'math'));
-}
-$result = curl_exec($ch);
-curl_close($ch);
-header('Access-Control-Allow-Origin: *');
-print($result);
 ?>
