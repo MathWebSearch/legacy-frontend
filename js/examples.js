c3ce112791e211a3b2be910a46260ad5e646b2ea
Corneliu C. Prodescu <cprodescu@gmail.com>
2014-08-20 20:07:55 +0200

    Re-enable examples and move to arxiv title.

--- js/examples.js
+++ js/examples.js
@@ -2,12 +2,12 @@
 //to not show examples, simply set MWS.examples to be an empty array. 
 MWS.examples = [
 //  Name                    Text                LaTeX Math
-//    ['Limit',                   'limit',        '\\lim_{?a\\rightarrow 0} ?x'],
-//    ['Fermat\'s Theorem',       'Fermat',       '?a^?n + ?b^?n=?c^?n'],
-//    ['Polynomial',              'polynomial',   'P(?x)'],
-//    ['Probability',             "probability",  'P(?x)'],
-//    ['Inequality chain',        'chain',        '0\\leq ?i\\leq ?n'],
-//    ['Lebesgue integral',       'Lebesgue',     '\\int_?a^?b |?f(?x)| d?x'],
-//    ['Signal Energy',           '',             '\\int_?a^?b (?f(x))^2 dx=?r'],
-//    ['Alpha-equality',          '',             '?a=_\\alpha ?b']
+    ['Limit',                   'limit',        '\\lim_{?a\\rightarrow 0} ?x'],
+    ['Fermat\'s Theorem',       'Fermat',       '?a^?n + ?b^?n=?c^?n'],
+    ['Polynomial',              'polynomial',   'P(?x)'],
+    ['Probability',             "probability",  'P(?x)'],
+    ['Inequality chain',        'chain',        '0\\leq ?i\\leq ?n'],
+    ['Lebesgue integral',       'Lebesgue',     '\\int_?a^?b |?f(?x)| d?x'],
+    ['Signal Energy',           '',             '\\int_?a^?b (?f(x))^2 dx=?r'],
+    ['Alpha-equality',          '',             '?a=_\\alpha ?b']
 ];
--- php/tema_proxy.php
+++ php/tema_proxy.php
@@ -1,6 +1,6 @@
 <?php
 
-define ('TEMA_URL', 'http://localhost:8901');
+define ('TEMA_URL', 'http://212.201.44.161:11005');
 
 $session = curl_init(TEMA_URL.'/?'.$_SERVER['QUERY_STRING']);
 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
