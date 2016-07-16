f9d808093d8282841d28cb0d2aed9d9dc553dfbf
alexglont <alex_glontzaru@yahoo.com>
2016-07-17 01:16:36 +0200

    new examples + MathJax url

--- js/examples.js
+++ js/examples.js
@@ -1,13 +1,9 @@
 //YOU MAY ENTER EXAMPELS IN THIS FILE
 //to not show examples, simply set MWS.examples to be an empty array. 
 MWS.examples = [
-//  Name                    Text                LaTeX Math
-    ['Limit',                   'limit',        '\\lim_{?a\\rightarrow 0} ?x'],
-    ['Fermat\'s Theorem',       'Fermat',       '?a^?n + ?b^?n=?c^?n'],
-    ['Polynomial',              'polynomial',   'P(?x)'],
-    ['Probability',             "probability",  'P(?x)'],
-    ['Inequality chain',        'chain',        '0\\leq ?i\\leq ?n'],
-    ['Lebesgue integral',       'Lebesgue',     '\\int_?a^?b |?f(?x)| d?x'],
-    ['Signal Energy',           '',             '\\int_?a^?b (?f(x))^2 dx=?r'],
-    ['Alpha-equality',          '',             '?a=_\\alpha ?b']
+//  Name                        Text                         LaTeX Math
+	['Permutation Characters',  'permutation characters',    '?c(?x)'        ],
+	['Power',                   'power',                     '?a^?b'         ],
+	['Polynomial',              'polynomial',                '?a+?b'         ],
+	['Eigenvalues',             'eigenvalues',               ''              ]
 ];
